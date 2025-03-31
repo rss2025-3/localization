73c89491b7cd26d03746ad150d3e10015df8235d
blammers32 <blammers32@gmail.com>
2025-03-31 20:07:56 +0000

    working motion model (maybe), with noise, not vectorized

--- localization/motion_model.py
+++ localization/motion_model.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 class MotionModel:
 
@@ -7,8 +7,7 @@ class MotionModel:
         # TODO
         # Do any precomputation for the motion
         # model here.
-
-        pass
+        self.noise_std = (0.05, 0.05, 0.05) # (x_noise, y_noise, theta_noise)
 
         ####################################
 
@@ -32,8 +31,36 @@ class MotionModel:
         """
 
         ####################################
-        # TODO
+        delta_x = odometry[0]
+        delta_y = odometry[1]
+        delta_theta = odometry[2]
 
-        raise NotImplementedError
+        updated_positions = np.zeros((len(particles), 3))
+        for i in range(len(particles)):
+            x = particles[i][0]
+            y =  particles[i][1]
+            theta = particles[i][2]
+
+            # Compute inverse rotation matrix
+            cos_theta = np.cos(theta)
+            sin_theta = np.sin(theta)
+            R_inv = np.array([[cos_theta, sin_theta, 0],
+                            [-sin_theta, cos_theta, 0], 
+                            [0, 0, 1]])
+            
+            # Transform displacement to body frame
+            body_frame = (R_inv @ np.array([x, y, theta]))
+            print(np.array([delta_x, delta_y, delta_theta]).T[0])
+            print(body_frame)
+            body_frame = body_frame + np.array([delta_x, delta_y, delta_theta]).T[0]
+
+            new_position = np.linalg.inv(R_inv) @ body_frame
+        
+            updated_positions[i, :] = new_position
+
+        noise = np.random.normal(scale=self.noise_std, size=updated_positions.shape)
+        updated_positions += noise
+        return updated_positions
+        
 
         ####################################
